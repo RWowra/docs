a3dc12c4cd57a9413f79b43ae24a94dba8f33e66
RWowra <wor9fe@bosch.com>
2025-02-13 11:56:36 +0100

    js upload

---
+++ js/versions.js
@@ -0,0 +1,6 @@
+// js/versions.js
+const versions = [
+    { version: 'V1', path: 'V1/COSYM_Online_Help_CSH.htm' },
+    { version: 'V2', path: 'V2/COSYM_Online_Help_CSH.htm' },
+    { version: 'Neueste Version', path: 'V2/COSYM_Online_Help_CSH.htm' }
+];
