90fc56069c50039d3a11f821075f9a9252d00c44
RWowra <wor9fe@bosch.com>
2025-02-13 12:32:42 +0100

    Folder Structure 3 up adapted for starting script

--- versions.js
+++ versions.js
@@ -2,5 +2,7 @@
 const versions = [
     { version: 'V1', path: '../V1/COSYM_Online_Help_CSH.htm' },
     { version: 'V2', path: '../V2/COSYM_Online_Help_CSH.htm' },
-	{ version: 'Neueste Version', path: '../V3/COSYM_Online_Help_CSH.htm' }
+	{ version: 'V3', path: '../3/COSYM_Online_Help_CSH.htm' },
+
+	{ version: 'Neueste Version', path: '../Current/COSYM_Online_Help_CSH.htm' }
 ];
