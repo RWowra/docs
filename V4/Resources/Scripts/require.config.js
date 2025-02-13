55528658c7a7a1902225e6ce1f45637de90e0b94
RWowra <wor9fe@bosch.com>
2025-02-13 13:24:02 +0100

    Upload V4

---
+++ V4/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=638507698558470499'
+});
--- versions.js
+++ versions.js
@@ -2,7 +2,7 @@
 const versions = [
     { version: 'V1', path: '../V1/COSYM_Online_Help_CSH.htm' },
     { version: 'V2', path: '../V2/COSYM_Online_Help_CSH.htm' },
-	{ version: 'V3', path: '../3/COSYM_Online_Help_CSH.htm' },
-
+	{ version: 'V3', path: '../V3/COSYM_Online_Help_CSH.htm' },
+	{ version: 'V4', path: '../V4/COSYM_Online_Help_CSH.htm' },
 	{ version: 'Neueste Version', path: '../Current/COSYM_Online_Help_CSH.htm' }
 ];
