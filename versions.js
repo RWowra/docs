2437f844fea712963553d6692e971aab792e35df
RWowra <wor9fe@bosch.com>
2025-02-13 12:24:30 +0100

    adaption for current folder

--- versions.js
+++ versions.js
@@ -1,6 +1,6 @@
 // js/versions.js
 const versions = [
-    { version: 'V1', path: 'V1/COSYM_Online_Help_CSH.htm' },
-    { version: 'V2', path: 'V2/COSYM_Online_Help_CSH.htm' },
-	{ version: 'Neueste Version', path: 'V3/COSYM_Online_Help_CSH.htm' }
+    { version: 'V1', path: '../V1/COSYM_Online_Help_CSH.htm' },
+    { version: 'V2', path: '../V2/COSYM_Online_Help_CSH.htm' },
+	{ version: 'Neueste Version', path: '../V3/COSYM_Online_Help_CSH.htm' }
 ];
