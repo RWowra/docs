60a2a2b4b9cf7cf80f8d83ecf92099410bc92d11
Obermueller Sophie (ETAS/EDO) <oob9fe@bosch.com>
2025-05-20 13:37:42 +0200

    fixed pagelayout landing

--- docs/Current/EN/Resources/Scripts/require.config.js
+++ docs/Current/EN/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638833404308317840'
+    urlArgs: 't=638833446839179331'
 });
--- docs/V1.2/EN/Resources/Scripts/require.config.js
+++ docs/V1.2/EN/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638833409013817678'
+    urlArgs: 't=638833446839179331'
 });
